27ee11c7ece0eec0ec421521eea4cfa2334a1dac
Juan <jflarm@gmail.com>
2020-08-04 14:14:17 -0400

    Agregamos el formulario para agregar peliculas en add-movies.php

--- pages/add_movies.php
+++ pages/add_movies.php
@@ -1,1 +1,22 @@
+<?php
+    include('../configs/config.php');
+?>
+
 <h1>Estas agregando peliculas en add-movies</h1>
+
+<form action="<?php echo PROJECT_PATH;?>'/sync/send_data.php" method="post">
+    <label for="titulo">Titulo</label>
+    <input type="text" id="titulo" name="titulo" placeholder="Titulo...">
+
+    <label for="director">Director</label>
+    <input type="text" id="director" name="director" placeholder="Director...">
+
+    <label for="genero">Genero</label>
+    <input type="text" id="genero" name="genero" placeholder="Genero...">
+
+    <label for="fecha">Fecha de Estreno</label>
+    <input type="text" id="fecha" name="fecha" placeholder="Fecha de Estreno...">
+
+    <button type="submit">Agregar</button>
+    <button type="reset">Cancelar</button>
+</form>
